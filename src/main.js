abea11a5f1924af261361de1415b7d2f474827f5
LeynaM <leynamay@gmail.com>
2024-11-30 20:15:02 +0000

    remove react

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import * as THREE from "three";
+
+const container = document.getElementById("container");
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const geometry = new THREE.BoxGeometry(4, 4, 0.5);
+const material = new THREE.MeshStandardMaterial({ color: 0x0ffff0 });
+material.roughness = 0.5;
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+scene.add(ambientLight);
+const pointLight = new THREE.PointLight(0xffffff, 100);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
+
+camera.position.z = 3;
+camera.position.y = -3;
+camera.rotation.x += 0.8;
+
+renderer.render(scene, camera);
+
+container.appendChild(renderer.domElement);
+container.addEventListener("mousemove", (event) => {
+  cube.rotation.y = event.clientX / window.innerWidth - window.innerWidth;
+  cube.rotation.x = event.clientY / window.innerHeight + window.innerHeight;
+  renderer.render(scene, camera);
+});
