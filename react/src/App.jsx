b89c92ddf2e3c52298c909881eeea28ea500fb5e
LeynaM <leynamay@gmail.com>
2025-02-13 18:04:46 +0000

    start game dialog box

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Flex, Text, Button } from "@radix-ui/themes";
 import { Game } from "./Game/Game";
+import StartGameDialog from "./components/StartGameDialog/StartGameDialog";
 
 let game;
 
@@ -10,11 +11,15 @@ function App() {
   const gameContainer = document.getElementById("game-container");
   if (!game) {
     game = new Game(gameContainer);
-    console.log("test");
   }
 
-  const [count, setCount] = useState(0);
-  return <></>;
+  const startGame = () => game.start();
+
+  return (
+    <>
+      <StartGameDialog onStart={startGame} />
+    </>
+  );
 }
 
 export default App;
---
+++ react/src/components/StartGameDialog/StartGameDialog.jsx
@@ -0,0 +1,50 @@
+import { Dialog, Button, Flex, Link } from "@radix-ui/themes";
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
+
+function StartGameDialog({ onStart }) {
+  const [open, setOpen] = useState(true);
+
+  const overrideClose = () => {
+    setOpen(true);
+  };
+
+  const start = () => {
+    onStart();
+    setOpen(false);
+  };
+
+  return (
+    <Dialog.Root open={open} onOpenChange={overrideClose}>
+      <Dialog.Content
+        maxWidth="450px"
+        onOpenAutoFocus={(e) => e.preventDefault()}
+      >
+        <Flex p="4" gap="6" direction="column">
+          <Dialog.Title size="8" align="center" mb="0">
+            Tilt Game
+          </Dialog.Title>
+          <Dialog.Description align="center" size="4">
+            Use the mouse to tilt the platform and guide the ball to the green
+            square. Don&apos;t roll into a hole or off the edge!
+            <br />
+            <Link
+              href="https://github.com/LeynaM/tilt-game"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Code available on GitHub. <ExternalLinkIcon />
+            </Link>
+          </Dialog.Description>
+          <Flex justify="center">
+            <Button onClick={() => start()} size="3">
+              Play
+            </Button>
+          </Flex>
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+}
+
+export default StartGameDialog;
