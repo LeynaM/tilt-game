fc409e3062adb80ce8b1012c6570c0419a813e12
LeynaM <leynamay@gmail.com>
2025-02-15 13:16:13 +0000

    added ranks to database

--- server/src/db/schema.js
+++ server/src/db/schema.js
@@ -3,7 +3,8 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const scoresTable = sqliteTable("scores_table", {
   id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  rank: integer().notNull(),
   player: text().notNull(),
   score: integer().notNull(),
-  timestamp: text().default(sql`(CURRENT_TIMESTAMP)`),
+  createdAt: integer().default(sql`(strftime('%s', 'now'))`),
 });
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,24 +1,46 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
 import { scoresTable } from "./db/schema.js";
+import { lt, gt, sql } from "drizzle-orm";
 
 const db = drizzle(process.env.DATABASE_URL);
 
 export default async function routes(fastify, options) {
   fastify.get("/api/scores", async (request, reply) => {
-    const allScores = await db.select().from(scoresTable);
+    const allScores = await db
+      .select()
+      .from(scoresTable)
+      .orderBy(scoresTable.createdAt, scoresTable.rank);
     return allScores;
   });
 
   fastify.post("/api/scores", async (request, reply) => {
     const { player, score } = request.body;
+
     fastify.log.info(request);
     try {
+      const newRank = await db
+        .$count(scoresTable, gt(scoresTable.score, score))
+        .then((count) => {
+          return count + 1;
+        });
+      fastify.log.info(newRank);
+
+      const updatedScores = await db
+        .update(scoresTable)
+        .set({ rank: sql`rank + 1` })
+        .where(lt(scoresTable.score, score))
+        .returning();
+
       const result = await db
         .insert(scoresTable)
-        .values({ player, score })
+        .values({ player, score, rank: newRank })
         .returning();
-      return result[0];
+
+      return {
+        score: result[0],
+        updatedScores,
+      };
     } catch (err) {
       fastify.log.error(err);
       reply.code(400).send({ error: "Score could not be created" });
